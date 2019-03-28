fa06eff5cdbae505e20462a2ee1b905313e8b626
HuynhTien <nhoktien1133@gmail.com>
2019-03-28 11:14:55 +0700

    trang 22

--- BigSchool-NHT/Controllers/CoursesController.cs
+++ BigSchool-NHT/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using BigSchool_NHT.Models;
 using BigSchool_NHT.Models.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace BigSchool_NHT.Controllers
             _dbContext = new ApplicationDbContext();
         }
         // GET: Courses
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
@@ -23,6 +25,27 @@ namespace BigSchool_NHT.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- BigSchool-NHT/Models/ViewModels/CourseViewModel.cs
+++ BigSchool-NHT/Models/ViewModels/CourseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,15 @@ namespace BigSchool_NHT.Models.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public String Place { get; set; }
+        [Required]
         public String Date { get; set; }
+        [Required]
         public String Time { get; set; }
+        [Required]
         public byte Category { get; set; }
+        [Required]
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime ()
         {
