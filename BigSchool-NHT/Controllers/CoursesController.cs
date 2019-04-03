ed9f5a870e110c5333f350ba625ec3a907090c6a
HuynhTien <nhoktien1133@gmail.com>
2019-04-03 10:16:55 +0700

    trang 51

--- BigSchool-NHT/Controllers/CoursesController.cs
+++ BigSchool-NHT/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@ using BigSchool_NHT.Models.ViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -47,6 +48,23 @@ namespace BigSchool_NHT.Controllers
             _dbContext.SaveChanges();
 
             return RedirectToAction("Index", "Home");
+        }
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var courses = _dbContext.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Course)
+                .Include(l => l.Lecturer)
+                .Include(l => l.Category)
+                .ToList();
+            var viewModel = new CourseViewModel
+            {
+                UpcommingCourses = courses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
     }
 }
