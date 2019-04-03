ef433b258e16e1dd825505b43e0a910a3ffcbe4a
HuynhTien <nhoktien1133@gmail.com>
2019-04-03 07:16:26 +0700

    trang 40

---
+++ BigSchool-NHT/Controllers/AttendancesController.cs
@@ -0,0 +1,33 @@
+﻿using BigSchool_NHT.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BigSchool_NHT.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }  
+        [HttpPost]
+        public IHttpActionResult Attend ([FromBody] int courseId)
+        {
+            var attendance = new Attendance
+            {
+                CourseId = courseId, 
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
